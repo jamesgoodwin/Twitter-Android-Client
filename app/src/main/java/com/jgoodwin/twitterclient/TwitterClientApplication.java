afcef80a8a6b17ee68915932e80ec608ce5f6e85
James Goodwin <jamesgoodwin@gmail.com>
2015-09-07 11:58:41 +0100

    Initialise Twitter & Fabric config

--- app/src/main/java/com/jgoodwin/twitterclient/TwitterClientApplication.java
+++ app/src/main/java/com/jgoodwin/twitterclient/TwitterClientApplication.java
@@ -2,12 +2,22 @@ package com.jgoodwin.twitterclient;
 
 import android.app.Application;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
 public class TwitterClientApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        TwitterAuthConfig authConfig =
+                new TwitterAuthConfig("u2RHSDMeY40mJooIcn06VJzgD",
+                        "nlATyUFhqvz4vk6bdEBACFPfHzB6c5nzI69Je476yKSNuXkNDs");
+
+        Fabric.with(this, new Twitter(authConfig));
     }
 
 }
